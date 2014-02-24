188e5ce4e5f6b587f50bcd79bcba2dee2de883c0
Graeme Hobbs <zer0bytes@users.onreply.github.com>
2014-02-25 01:22:51 +1000

    [zer0bytes] Removed GPG Suite due to installation errors.

--- modules/people/manifests/zer0bytes.pp
+++ modules/people/manifests/zer0bytes.pp
@@ -2,8 +2,5 @@ class people::zer0bytes {
 	packages {
 		'tree': ;
 		'wget': ;
-		'GPGSuite':
-			provider => 'appdmg',
-			source => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg'
 	}
 }
