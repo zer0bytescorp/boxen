4f4fcf95219b2f13502b40b7ef18f6469aa59721
Graeme Hobbs <zer0bytes@users.onreply.github.com>
2014-02-24 21:03:52 +1000

    [zer0bytes] Added personal manifest with wget, tree and GPG suite listed.

---
+++ modules/people/manifests/zer0bytes.pp
@@ -0,0 +1,9 @@
+class people::zer0bytes {
+	packages {
+		'tree': ;
+		'wget': ;
+		'GPGSuite':
+			provider => 'appdmg',
+			source => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg'
+	}
+}
