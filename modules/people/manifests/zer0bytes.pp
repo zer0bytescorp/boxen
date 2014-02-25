110f0a86b1e970e51ee63141da2bb191b92afa89
Graeme Hobbs <zer0bytes@users.onreply.github.com>
2014-02-25 23:32:48 +1000

    Added Mac OSX defaults

--- modules/people/manifests/zer0bytes.pp
+++ modules/people/manifests/zer0bytes.pp
@@ -8,4 +8,17 @@ class people::zer0bytes {
 		'tree': ;
 		'wget': ;
 	}
+	
+	boxen::osx_defaults {
+		'Show battery percentage':
+			user		=> $::luser,
+			domain	=> 'com.apple.menuextra.battery',
+			key		=> 'ShowPercent',
+			value	=> true;
+		'Require fn for media keys':
+			user		=> $::luser,
+			domain	=> 'NSGlobalDomain',
+			key		=> 'com.apple.keyboard.fnState',
+			value	=> 1;
+	}
 }
